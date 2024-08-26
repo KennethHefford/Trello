9abf9850b433516a552a46d5ae5d03f4e9378c74
Kenneth Hefford <kennethhefford@outlook.com>
2024-08-26 14:50:51 +1000

    Created User Model and Schema

---
+++ models/user.py
@@ -0,0 +1,25 @@
+from init import db, ma
+
+
+class User(db.Model):
+    # name of the table
+    __tablename__ = "users"
+
+    # attributes of the table
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+# used to fetch information from db for dump
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "email", "passwords", "is_admin")
+
+# to handle a single user object
+user_schema = UserSchema(exclude=["password"])
+# to handle a list of user objects
+users_schema = UserSchema(many=True, exclude=["password"])
+
+
